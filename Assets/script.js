cc66b9024388269dbc8e8dc2b7177cf66c231d46
Michel Kong <minkya_u@daum.net>
2019-10-23 11:50:34 -0700

    sucessful in adding the city name on the list

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,11 @@
-var search
+var searchedCitiesList = document.querySelector('#searchedCities');
+
+searchBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    var searchedCity = document.querySelector('#searchedCity').value;
+    var resultCity = document.createElement('li');
+    resultCity.setAttribute('class', 'collection-item');
+    resultCity.append(searchedCity);
+    searchedCitiesList.append(resultCity);
+})
+
